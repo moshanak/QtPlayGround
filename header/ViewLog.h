f1224acbf21edf8c6073c8b63c45450f849702e4
moshanak <moshamosha2@yahoo.co.jp>
2022-03-28 20:43:58 +0900

    [update] 別マシンで作業するためにコミット

--- header/ViewLog.h
+++ header/ViewLog.h
@@ -5,9 +5,19 @@
 class ViewLog : public ViewBase
 {
     Q_OBJECT
+    Q_PROPERTY(qreal t READ t WRITE setT NOTIFY tChanged)
 public:
     ViewLog() = delete;
     explicit ViewLog(QQuickView* parent);
     void initialize();
+
+    qreal t() const { return m_t; }
+    void setT(qreal t);
+
+signals:
+    void tChanged();
+
+private:
+    qreal m_t;
 };
 #endif //VIEWLOG_H
--- source/ViewLog.cpp
+++ source/ViewLog.cpp
@@ -2,6 +2,7 @@
 
 ViewLog::ViewLog(QQuickView* parent)
 	:ViewBase(parent)
+	, m_t(0)
 {
 }
 
@@ -10,4 +11,13 @@ void ViewLog::initialize()
 	setResizeMode(QQuickView::SizeRootObjectToView);
 	setSource(QUrl("qrc:///ViewLog.qml"));
 	show();
+}
+
+void ViewLog::setT(qreal t)
+{
+	if (m_t == t) {
+		return;
+	}
+	m_t = t;
+	emit tChanged();
 }
